792d602c4390c1e5efeacaac6d784c93e2dfc645
ElRaptorus <christian.werner@5minds.de>
2019-07-04 10:47:34 +0200

    :sparkles: Add optional 'enabled' to TimerEventDefintion with default set to true

--- src/types/model/events/definitions/timer_event_definition.ts
+++ src/types/model/events/definitions/timer_event_definition.ts
@@ -2,6 +2,7 @@ import {EventDefinition} from './event_definition';
 
 export class TimerEventDefinition extends EventDefinition {
 
+  public enabled?: boolean = true;
   public timerType: TimerType;
 
 }
